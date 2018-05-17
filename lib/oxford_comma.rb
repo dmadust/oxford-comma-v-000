50485e1532f86e5d959e6a7ad75f5a84263ddfe5
dmadust@gmail.com <dmadust@gmail.com>
2018-05-17 20:49:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,8 @@ def oxford_comma(array)
     array[0]
   elsif array.count == 2
     "#{array[0]} and #{array[1]}"
-  array[-1] = "and " << array[-1]
-  array.join(", ")
+  else
+    array[-1] = "and " << array[-1]
+    array.join(", ")
+  end
 end
